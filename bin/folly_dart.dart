cc5e1c87bc1ed69fd351516d3fd188512e780184
MAC <babatundeyusuffolorunsho@gmail.com>
2025-06-03 18:28:22 +0100

    dart for flutter beginner

---
+++ bin/folly_dart.dart
@@ -0,0 +1,17 @@
+import 'package:folly_dart/folly_dart.dart' as folly_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${folly_dart.calculate()}!');
+  var age = 12;
+  if(age >= 12){
+    print("afge is $age");
+  }else{
+    print("age os");
+  }
+
+  follyb(); 
+}
+
+void follyb() {
+  print("Hello from follyb!");
+}
---
+++ test/folly_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:folly_dart/folly_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
